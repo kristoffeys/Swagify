0932d1d98f80d9bccb40968a799f5cb3f6a1a7a4
Kristof Feys <kristof.feys@me.com>
2013-12-03 22:04:24 +0100

    Added doctype + hashtags!

--- bookmarklet.js
+++ bookmarklet.js
@@ -16,4 +16,39 @@ javascript: (function () {
     a.setAttribute('type', 'image/png');
     a.setAttribute('href', 'data:image/x-icon;base64,AAABAAEAEBAQAAAAAAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAACo8KAP8ALwAAAP8A6gD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADMzABACAkREAAMBEQICQAQAAxEREgJABAADEBASAkAEAAMQEBICQEQzMxAQEgJAADAAEBASIkAAMAAQABICQAAwABAAEgJAADAAEAASAkAAMAAQABICQAQzMxAAEiJERAAAAAAAAAAAAAAAAAAAAAD//wAA//8AAA2gAADopgAA4CYAAOUmAADlJAAABScAAHUHAAB3JwAAdycAAHcnAAB3JgAABwAAAP//AAD//wAA');
     document.getElementsByTagName("head")[0].appendChild(a);
+
+    //change doctype
+    document.write('<!DOCTYPE YOLO>\n' + document.head.outerHTML + document.body.outerHTML);
+
+    //add hashtags!
+    var walkDOM = function (node, func) {
+        func(node);
+        node = node.firstChild;
+        while(node) {
+            walkDOM(node, func);
+            node = node.nextSibling;
+        }
+
+    };
+    walkDOM(document.body, function (node) {
+
+        if (node.nodeName === '#text') {
+            var text = node.textContent;
+            var tmp = "";
+            text = text.replace(/[^A-Za-z]/g, ' ');
+
+            text = text.split(' ');
+
+            if (text.length) {
+                for (var i = 0, length = text.length; i < length; i += 1) {
+                    var word = text[i];
+                    if(word.length > 1){
+                        tmp += '#' + word + ' ';
+                    }
+                }
+            }
+            node.nodeValue = tmp;
+        }
+    });
+
 })()
